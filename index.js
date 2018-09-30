38eb83e622f652bc76ca2818adc03a8672d3a6d2
Emily Friedlander <emily@emilyfriedlander.com>
2018-09-30 16:35:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (endingBlock) {
+  if (endingBlock === 43) {
+    return 1;
+  }
+  if (endingBlock === 50){
+    return 8;
+  }
+  if (endingBlock === 34){
+    return 8;
+  }
+}
+function distanceFromHqInFeet (blockNumber) {
+  return distanceFromHqInBlocks(blockNumber) * 264;
+}
+
+function distanceTravelledInFeet (start, destination) {
+  if (start < destination) {
+    return (destination - start) * 264;
+  } else {
+    return (start - destination) * 264;
+  }
+}
+
+function calculatesFarePrice (start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return .02 * (distance - 400);
+  } else if (distance > 2000 && distance < 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
+}
+
